6ac53650804ece4d4b59c2c723a81937a5d78145
AnuragKumar <anuragkumarrr@gmail.com>
2021-06-22 09:18:06 -0500

    adding local storage method

--- todos-list/src/App.js
+++ todos-list/src/App.js
@@ -2,10 +2,17 @@ import './App.css';
 import Header from "./MyComponents/Header"
 import Todos from "./MyComponents/Todos"
 import {Footer} from "./MyComponents/Footer"
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import {AddTodo} from "./MyComponents/AddTodo"
 
 function App() {
+  let initTodo;
+if( localStorage.getItem("todos")===null){
+  initTodo =[]
+}else{
+  initTodo = JSON.parse(localStorage.getItem("todos"))
+}
+
 
   const onDelete = (todo)=>{
     console.log('You have reached onDelete function',todo)
@@ -13,25 +20,33 @@ function App() {
     setTodos(todos.filter((e)=>{
       return e!==todo;
     }));
+    localStorage.setItem("todos",JSON.stringify(todos))
   }
  
   const addTodo = (title,desc)=>{
     console.log('I\'m adding this todo',title,desc)
-    let sno = todos[todos.length-1].sno+1
-            const myTodo = {
+    let sno;
+    if(todos.length===0){
+      sno = 1;
+    }else{
+      sno = todos[todos.length-1].sno+1
+    }
+    
+    const myTodo = {
                 sno   : sno,
                 title : title,
                 desc  : desc
             }
             setTodos([...todos,myTodo])
             console.log(myTodo)
+
   }
 
-  const [todos,setTodos] = useState([
-    {sno:1,title:"go to the market",desc:"You need to go to market for shopping"},
-    {sno:2,title:"go to the mall",desc:"You need to go to mall for window shopping"},
-    {sno:3,title:"go to the restaurant",desc:"You need to go to restaurant for eating"}
-  ]);
+  const [todos,setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos",JSON.stringify(todos))}, [todos])
+
+
 
   return (
     <div>
--- todos-list/src/MyComponents/Todos.js
+++ todos-list/src/MyComponents/Todos.js
@@ -2,13 +2,18 @@ import React from 'react'
 import {Todoitems} from './Todoitems'
  
 const Todos = (props) => {
+
+    let footerStyle={
+        minHeight:"40vh",
+        margin: "10px auto",
+    }
     return (
-        <div className="container">
+        <div className="container" style={footerStyle}>
             <h3 className="my-3">Todo Lists</h3>
             {
                 props.todos.length===0?"No todos to display":
             props.todos.map((todo)=>{
-                return <Todoitems todo={todo} key={todo.sno} onDelete={props.onDelete}/>
+                return (<><hr/><Todoitems todo={todo} key={todo.sno} onDelete={props.onDelete}/></>)
             }
             )
          }
